0b69774e4f90159a1c375e1e37af2a6cb9322d7d
neha <neha3221015@gmail.com>
2025-02-11 00:35:02 +0530

    added servletInitializer class in main class

--- src/main/java/com/example/jspPage/JspPageApplication.java
+++ src/main/java/com/example/jspPage/JspPageApplication.java
@@ -2,9 +2,15 @@ package com.example.jspPage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JspPageApplication {
+public class JspPageApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JspPageApplication.class);
+    } 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JspPageApplication.class, args);
